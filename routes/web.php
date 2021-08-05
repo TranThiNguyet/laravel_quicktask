dae3c5c71c16a83d54fc92a3f008af77084f189c
TranThiNguyet <nguyettran092@gmail.com>
2021-08-05 22:05:43 +0700

    Database migrations, Eloquent Models, Routing, Building Layouts and Views

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Http\Request;
+use App\Http\Controllers\LanguageController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('tasks'); 
+})->name('/');
+
+Route::get('change-language/{language}', [LanguageController::class, 'changeLanguage'])->name('change-language');
+
+Route::resource('tasks', 'TaskController');
